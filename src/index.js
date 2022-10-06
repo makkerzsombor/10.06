7759403f16c0a5f858414e982ee88c2da75f5eaa
Zsombor Makker <makker.zsombor@gmail.com>
2022-10-06 12:41:01 +0200

    Plusz feladatokkal együtt készen van

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const book_1 = require("./book");
 let lista = [];
+let lista2 = [];
 lista.push(new book_1.Book('Film_1', 5));
 lista.push(new book_1.Book('Film_2', 8));
 lista.push(new book_1.Book('Film_3', 10));
@@ -16,5 +17,25 @@ for (let i = 0; i < lista.length; i++) {
     }
 }
 console.log('');
+console.log('Plusz feladatok');
 console.log('');
-console.log('');
+for (let i = 0; i < 30; i++) {
+    let rnd = Math.floor(Math.random() * 10 + 1);
+    lista2.push(new book_1.Book('Book#' + i, rnd));
+}
+console.log('Második lista: ');
+for (let i = 0; i < lista2.length; i++) {
+    console.log(lista2[i].toString());
+}
+function bestof() {
+    let index = 0;
+    let best = 0;
+    for (let i = 0; i < lista2.length; i++) {
+        if (lista2[i].rating > best) {
+            best = lista2[i].rating;
+            index = i;
+        }
+    }
+    console.log('A legjobb értékelésű film: ' + lista2[index].name + ' értékelése: ' + best);
+}
+bestof();
--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,46 @@
 import { Book } from "./book";
 
 let lista : Book[] = [];
+let lista2 : Book[] = [];
 
 lista.push(new Book('Film_1',5));
 lista.push(new Book('Film_2',8));
 lista.push(new Book('Film_3',10));
 
 console.log('filmek: ')
-for (let i = 0; i < lista.length; i++) {
+for (let i:number = 0; i < lista.length; i++) {
     console.log(lista[i].name);
 }
 
 console.log('7-nél nagyobb értékeléssel rendelkező filmek: ');
-for (let i = 0; i < lista.length; i++) {
+for (let i:number = 0; i < lista.length; i++) {
     if (lista[i].rating > 7) {        
     console.log('A film címe: '+lista[i].name + 'a film értékelése: '+lista[i].rating);
     }
 }
 
 console.log('');
-console.log('');
+console.log('Plusz feladatok');
 console.log('');
 
+for (let i = 0; i < 30; i++) {
+        let rnd:number = Math.floor(Math.random()* 10 + 1) ;
+        lista2.push(new Book('Book#'+i , rnd));
+}
+console.log('Második lista: ');
+for (let i = 0; i < lista2.length; i++) {
+    console.log(lista2[i].toString());
+}
 
+function bestof() {
+    let index:number =0;
+    let best:number = 0;
+    for (let i = 0; i < lista2.length; i++) {
+        if (lista2[i].rating > best) {
+            best = lista2[i].rating;
+            index = i;
+        }
+    }
+    console.log('A legjobb értékelésű film: '+lista2[index].name+' értékelése: '+best);
+}
+bestof();
